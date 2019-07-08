daf8a02f6757a723bac86ac6b09b8d91d18f923a
Mathavan_K <mathavan_k@infosys.com>
2019-07-08 12:45:22 +0530

    push code into the master branch - initialize the project module

---
+++ app/src/main/java/com/cvapp/assignment/views/HomeScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.cvapp.assignment.views
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.cvapp.assignment.R
+
+class HomeScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen)
+    }
+}
