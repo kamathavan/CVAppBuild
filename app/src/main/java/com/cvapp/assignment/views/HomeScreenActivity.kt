5272c108079a3b82cb68ecccfd87597e317e55bc
Mathavan_K <mathavan_k@infosys.com>
2019-07-08 14:10:52 +0530

    push code to dev

--- app/src/main/java/com/cvapp/assignment/views/HomeScreenActivity.kt
+++ app/src/main/java/com/cvapp/assignment/views/HomeScreenActivity.kt
@@ -9,5 +9,6 @@ class HomeScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
+        // this ok
     }
 }
